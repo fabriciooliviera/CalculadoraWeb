87a0cbeeebfc1d9e03b9fdf517efd6fa9fe33fff
fabriciooliviera <38145209+fabriciooliviera@users.noreply.github.com>
2018-11-29 19:54:40 -0200

    Add files via upload

---
+++ arquivo.js
@@ -0,0 +1,16 @@
+    
+        function calcular(){
+  
+          var valor1 = document.getElementById("valor1").value;
+          var valor2 = document.getElementById("valor2").value;
+          var sinal = document.getElementById("sinal").value;
+          var resultado;
+  
+          if(sinal=="+") resultado = parseInt(valor1) + parseInt(valor2);
+          if(sinal=="-") resultado = parseInt(valor1) - parseInt(valor2);
+          if(sinal=="*") resultado = parseInt(valor1) * parseInt(valor2);
+          if(sinal=="/") resultado = parseInt(valor1) / parseInt(valor2);
+  
+          document.getElementById("resultado").innerHTML = resultado;
+            return resultado;
+        }
